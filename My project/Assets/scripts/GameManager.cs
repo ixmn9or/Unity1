a639f73cd977b29f9b8d05ce146594b3c50228db
ixmn9or <ixmn9or@gmail.com>
2023-09-07 22:28:51 -0400

    week 3 game is done

---
+++ My project/Assets/scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    int score = 0;
+    bool isGameOver = false;
+
+    public static GameManager instance;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject gameOverText;
+
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") && isGameOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void InitiateGameOver()
+    {
+        isGameOver = true;
+        gameOverText.SetActive(true);
+    }
+
+}
